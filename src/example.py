45282ee5db8f40025c99d4e1ffac31d7ba48776f
abuddenberg <andrew.buddenberg@noaa.gov>
2014-01-23 11:25:15 -0500

    Fixed example code. .gitignore'd .pyc files for real this time

--- src/example.py
+++ src/example.py
@@ -5,11 +5,11 @@ base_url = 'http://data.gcis-dev-front.joss.ucar.edu'
 gcis = GcisClient(base_url, 'andrew.buddenberg@noaa.gov', 'fcee8e3f11f36313e463ece51aab15242f71f3d552d565be')
 
 # #Let's pull a list of all figures in Chapter 2 of the NCA3draft
-# for partial_figure in gcis.get_figure_listing('nca3draft', chapter_id='our-changing-climate'):
-#     #The listing doesn't provide all available fields for the figure (Images, for instance).
-#     #There aren't very many figures, so let's go ahead and grab a complete version of each
-#     full_figure = gcis.get_figure('nca3draft', partial_figure.identifier, chapter_id='our-changing-climate')
-#     print full_figure
+for partial_figure in gcis.get_figure_listing('nca3draft', chapter_id='our-changing-climate'):
+    #The listing doesn't provide all available fields for the figure (Images, for instance).
+    #There aren't very many figures, so let's go ahead and grab a complete version of each
+    full_figure = gcis.get_figure('nca3draft', partial_figure.identifier, chapter_id='our-changing-climate')
+    print full_figure
 
 
 #Let's work with the infamous temperature figure
@@ -24,4 +24,10 @@ print fig2_6.original
 #Let's see about the images:
 for image in fig2_6.images:
     print image
+
+#How about the whole Image?
+print gcis.get_image('69da6d93-4426-4061-a2a1-7b3d01f2dc1c').as_json(indent=4)
+
+
+
 
