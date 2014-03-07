86de838c08132536ca3db47038043a01bd44324c
abuddenberg <andrew.buddenberg@noaa.gov>
2014-03-07 09:55:05 -0500

    Minor changes to variable names

--- src/example.py
+++ src/example.py
@@ -13,29 +13,24 @@ for partial_figure in gcis.get_figure_listing('nca3draft', chapter_id='our-chang
 
 
 #Let's work with the infamous temperature figure
-fig2_6 = gcis.get_figure('nca3draft', 'observed-us-temperature-change')
+fig2_7 = gcis.get_figure('nca3draft', 'observed-us-temperature-change')
 
 #Warning: Images and Chapters are specifically excluded from JSON output.  This is what gets sent to GCIS. So...
-print fig2_6.as_json(indent=4)
+print fig2_7.as_json(indent=4)
 
 #...you might want to view the original input
-print fig2_6.original
+print fig2_7.original
 
 #Let's see about the images:
-for image in fig2_6.images:
+for image in fig2_7.images:
     print image
 
 #How about the whole Image?
 print gcis.get_image('69da6d93-4426-4061-a2a1-7b3d01f2dc1c').as_json(indent=4)
 
 #Let's assign some GCMD keywords (The first 4 from the list)
-keyword_ids = [k['identifier'] for k in gcis.get_keyword_listing()[0:4]]
+# keyword_ids = [k['identifier'] for k in gcis.get_keyword_listing()[0:4]]
 
-for keyword_id in keyword_ids:
-    resp = gcis.associate_keyword_with_figure(keyword_id, 'nca3draft', 'observed-us-temperature-change')
-    print resp.status_code, resp.text
-
-
-
-
-
+# for keyword_id in keyword_ids:
+#     resp = gcis.associate_keyword_with_figure(keyword_id, 'nca3draft', 'observed-us-temperature-change')
+#     print resp.status_code, resp.text
