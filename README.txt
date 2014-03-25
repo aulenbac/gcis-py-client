fa4f0225618ffd43c0ce073ca46fe4f5972b8162
abuddenberg <andrew.buddenberg@noaa.gov>
2014-03-25 14:56:34 -0400

    Refactoring package structure

---
+++ README.txt
@@ -0,0 +1,10 @@
+gcis-py-client
+===========
+
+gcis-py-client provides a python-based abstraction of the Global Change
+Information System Structured Data Server [API](http://data.globalchange.gov).
+
+Usage
+-----
+
+More to follow.  This is my first distributable python package.
---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='GcisPyClient',
+    version='0.67',
+    author='Andrew Buddenberg',
+    author_email='andrew.buddenberg@noaa.gov',
+    packages=['gcis-py-client', 'gcis-py-client.test'],
+    scripts=['bin/example.py'],
+    url='http://data.globalchange.gov',
+    description='CLient for GCIS webservices',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        "requests >= 2.1.0",
+    ],
+)
